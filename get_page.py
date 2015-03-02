42329c146b260d71b4b029d1f7e2175c1db85827
Alexander Schepanovski <suor.web@gmail.com>
2015-03-02 20:04:42 +0700

    Review get_page.py

--- get_page.py
+++ get_page.py
@@ -1,10 +1,14 @@
+# -*- coding: utf-8 -*-
 import sys
 import urllib
 
+# R: в этой функции для отступов используются табы, используй пробелы,
+#    также настрой Sublime, чтобы конвертировал их автоматически 
 def get_page(url):
 	page = urllib.urlopen(url)
 	content = page.read()
 	page.close()
 	return content
 
-print get_page(sys.argv[1]) 
+# R: нет проверки, что урл передан, просто `python get_page.py` вызовет ошибку
+print get_page(sys.argv[1])
