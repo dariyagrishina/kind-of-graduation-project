50865081703a19752bdb2c058fdc7ff64091999e
Dariya Grishina <dvgrischina@mail.ru>
2015-03-02 21:16:25 +0700

    fix error caused argument failure

--- get_page.py
+++ get_page.py
@@ -1,10 +1,15 @@
 import sys
 import urllib
 
-def get_page(url):
-	page = urllib.urlopen(url)
-	content = page.read()
-	page.close()
-	return content
+def get_page(argument):
+    if len(argument) == 1:
+        return "Please, enter url"
+    else:
+        url = argument[1] 
+        page = urllib.urlopen(url)
+        content = page.read()
+        page.close()
+    return content 
+    
 
-print get_page(sys.argv[1]) 
+print get_page(sys.argv) 
