5572f174e297ec058937d206ec6e2b28d943097e
Dariya Grishina <dvgrischina@mail.ru>
2015-03-02 19:18:10 +0700

    write get_page function

--- get_page.py
+++ get_page.py
@@ -1,1 +1,10 @@
-# Здесь будет код
+import sys
+import urllib
+
+def get_page(url):
+	page = urllib.urlopen(url)
+	content = page.read()
+	page.close()
+	return content
+
+print get_page(sys.argv[1]) 
