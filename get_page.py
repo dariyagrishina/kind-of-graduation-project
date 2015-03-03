76902bdcf566febf3faf2013b6e5591590b1d71a
Alexander Schepanovski <suor.web@gmail.com>
2015-03-03 09:54:00 +0700

    Apply get_page.py fixes review

--- get_page.py
+++ get_page.py
@@ -1,20 +1,16 @@
 import sys
-import urllib # R: в питоне между блоками верхнего уровня принято ставить 2 пустых строки
+import urllib
 
-def get_page(argument):
-    # R: эта проверка не имеет отношения к этой функции и должна выполняться снаружи.
-    #    Общее правило: одна функция - одна задача. Сейчас тут 2.
-    if len(argument) == 1:
-        # R: а если на странице написано "Please, enter url", то мы не почувствуем разницы :)
-        #    Такие приколы как раз причина общего правила выше.
-        return "Please, enter url" 
-    else:
-        url = argument[1] 
-        page = urllib.urlopen(url)
-        content = page.read()
-        page.close()
-    return content # R: по логике это должно быть в else
-# R: на следующей строке 4 пробела, и кое-где ещё они разбросаны
-    
 
-print get_page(sys.argv) 
+def get_page(url):
+    page = urllib.urlopen(url)
+    content = page.read()
+    page.close()
+    return content
+
+
+if len(sys.argv) < 2:
+    print "\nUSAGE: python get_page.py <page-url>\n"
+    sys.exit(1)
+else:
+    print get_page(sys.argv[1])
