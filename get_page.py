49c50cd79395cdfb0e786c16a7815f74e5815d82
Dariya Grishina <dvgrischina@mail.ru>
2015-03-04 22:21:00 +0700

    add not full get_title
    Conflicts:
    	get_page.py

--- get_page.py
+++ get_page.py
@@ -1,3 +1,7 @@
+#!/usr/bin/python -tt
+# -*- coding: utf-8 -*-
+
+
 import sys
 import urllib
 
@@ -9,8 +13,18 @@ def get_page(url):
     return content
 
 
-if len(sys.argv) < 2:
-    print "\nUSAGE: python get_page.py <page-url>\n"
-    sys.exit(1)
-else:
-    print get_page(sys.argv[1])
+#if len(sys.argv) < 2:
+#    print "\nUSAGE: python get_page.py <page-url>\n"
+#    sys.exit(1)
+#else:
+#    print get_page(sys.argv[1])
+
+
+def get_title(film_number):
+    page_content = get_page("http://www.kinopoisk.ru/film"+str(film_number)+"/")
+    title_beginn = page_content.find("<title>")
+    title_end = page_content.find("</title>", title_beginn)
+    title = page_content[title_beginn + 7:title_end]
+    return title
+
+print get_title(401)
