17c1bbaaf45220ca4045723626504ae1b4be6af2
Dariya Grishina <dvgrischina@mail.ru>
2015-03-07 18:32:50 +0700

    modify code according to review #2

--- get_page.py
+++ get_page.py
@@ -6,45 +6,20 @@ import sys
 import urllib
 
 
-# R: стандартная практика - функции внутри программы работают с логическими строками,
-#    и преобразование происходит при вводе и выводе.
-#    т.е. get_title() как внутренняя функция вообще ничего не должна знать о кодировках.
-#
-#    План такой:
-#       - get_page() получает бинарную строку из page.read() и тут же её декодирует,
-#       - get_title() получает страницу в декодированном виде и выдаёт название фильма,
-#         также в декодированном виде,
-#       - код вызывающий get_title() ответственен за кодирование результата если хочет его вывести.
-#
-#    Это хорошо потому что
-#       - с таким get_page() легче работать,
-#       - результат get_title() может и не пойти на консоль, а подвергнутся дальнейшей обработке.
-
-
 def get_page(url):
     page = urllib.urlopen(url)
-    content = page.read()
+    content_cp1251 = page.read()
+    content = content_cp1251.decode('cp1251')
     page.close()
     return content
 
 
 def get_title(film_number):
-    # R: используй форматирование строк вместо конкатенации -
-    #    http://pythonworld.ru/osnovy/formatirovanie-strok-operator.html
-    page_content = get_page("http://www.kinopoisk.ru/film" + str(film_number) + "/")
-
-    # R: title_beginn -> title_begin
-    title_beginn = page_content.find("<title>")
-    title_end = page_content.find("</title>", title_beginn)
-    # R: лучше не использовать одно и то же имя для разных вещей
-    #    title_cp1251 = page_content[...]
-    #    title = title_1251.decode(...)
-    #    title_utf8 = title.encode(...)
-    title = page_content[title_beginn + 7:title_end]
-    title = title.decode('cp1251')
-    title = title.encode("utf8")
-    return title # получился, на самом деле, совсем не фильм, но ввиду блокировки не могу дальше практиковаться
-
+    page_content = get_page("http://www.filmz.ru/film/%s/" % film_number)
+    title_begin = page_content.find("<title>Filmz.ru")
+    title_end = page_content.find("(", title_begin)
+    title = page_content[title_begin + 17:title_end]
+    return title
 
 
 if len(sys.argv) < 2:
