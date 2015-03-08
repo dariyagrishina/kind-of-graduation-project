20e8490125279bcbd6e60f9dad4463b004c39167
Alexander Schepanovski <suor.web@gmail.com>
2015-03-08 14:46:49 +0700

    Apply #2 fixes review

--- get_title.py
+++ get_title.py
@@ -22,14 +22,7 @@ def get_title(film_number):
     return title
 
 
-# R: аргументы коммандной строки всегда приходят как строки,
-#    поэтому type(sys.argv[1]) == str всегда
-# R: type(x) возвращает тип (класс), а не строку
-#    поэтому стоит проверять type(var) is int (в данном случае там всё равно строка)
-#    ещё лучше isinstance(var, int)
-# R: правильно в данном случае было бы проверить, что в аргументе только цифры
-#      var.isdigit()
-if len(sys.argv) < 2 or type(sys.argv[1]) != "<type 'int'>":
+if len(sys.argv) < 2 or not sys.argv[1].isdigit():
     print "\nUSAGE: python get_title.py <film-id>\n"
     sys.exit(1)
 else:
