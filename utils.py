806264ee1acc026c14da0565300ca80bf8ab5473
Alexander Schepanovski <suor.web@gmail.com>
2015-03-10 13:09:39 +0700

    Add a TODO on detecting page encoding

--- utils.py
+++ utils.py
@@ -3,6 +3,7 @@ import urllib
 
 def get_page(url):
     page = urllib.urlopen(url)
+    # TODO: detect encoding
     content_cp1251 = page.read()
     content = content_cp1251.decode('cp1251')
     page.close()
