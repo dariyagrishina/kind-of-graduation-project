708945576733905119da67a81e8e5446a6e2efdd
Dariya Grishina <dvgrischina@mail.ru>
2015-03-10 23:03:02 +0700

    Use BeautifulSoup

--- get_title.py
+++ get_title.py
@@ -2,14 +2,18 @@
 # -*- coding: utf-8 -*-
 import sys
 import utils
+from bs4 import BeautifulSoup
+
 
 
 def get_title(film_number):
     page_content = utils.get_page("http://www.filmz.ru/film/%s/" % film_number)
-    title_begin = page_content.find("<title>Filmz.ru")
-    title_end = page_content.find("(", title_begin)
-    title = page_content[title_begin + 17:title_end]
-    return title
+    soup = BeautifulSoup(page_content)
+    soup.prettify()
+    h1_line = soup.h1
+    span_line = h1_line.span
+    text = span_line.get_text()
+    return text
 
 
 if len(sys.argv) < 2 or not sys.argv[1].isdigit():
