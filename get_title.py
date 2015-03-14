9bf46d250613bfd316ece1f17acd5616f56ba499
Dariya Grishina <dvgrischina@mail.ru>
2015-03-14 18:40:28 +0700

    =Make get_title.py take several film ids

--- get_title.py
+++ get_title.py
@@ -5,17 +5,30 @@ import utils
 from bs4 import BeautifulSoup
 
 
-def get_title(film_number):
-    page_content = utils.get_page("http://www.filmz.ru/film/%s/" % film_number)
-    soup = BeautifulSoup(page_content)
-    h1 = soup.h1
-    span = h1.span
-    text = span.get_text()
-    return text
+def get_title(film_number_list):
+    film_names_list = []
+    for film_number in film_number_list:
+        page_content = utils.get_page("http://www.filmz.ru/film/%s/" % film_number)
+        soup = BeautifulSoup(page_content)
+        h1 = soup.h1
+        span = h1.span
+        text = span.get_text()
+        film_names_list.append(text)
+    return film_names_list
 
 
-if len(sys.argv) < 2 or not sys.argv[1].isdigit():
+#agruments_list_length = len(sys.argv)
+film_number_list = []
+if len(sys.argv) < 2:
     print "\nUSAGE: python get_title.py <film-id>\n"
     sys.exit(1)
-else:
-    print get_title(sys.argv[1])
+for index in range(1, len(sys.argv)):
+    if not sys.argv[index].isdigit():
+        print "\nUSAGE: python get_title.py <film-id>\n"
+        sys.exit()
+    else:
+        film_number_list.append(sys.argv[index])
+
+titles = get_title(film_number_list)
+for title in titles:
+    print title
