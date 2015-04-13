ed094bef33c367c4fb7b0edea15b16b5191489fb
Dariya Grishina <dvgrischina@mail.ru>
2015-04-13 21:54:36 +0700

    Addressed issue #12

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
@@ -6,6 +6,6 @@ app = Flask(__name__)
 def first_page():
     return render_template("index.html")
 
-
+app.debug = True
 if __name__ == '__main__':
     app.run()
