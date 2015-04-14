d0e9ec3c6674195992b0a7e1f0505613b6dd1293
Alexander Schepanovski <suor.web@gmail.com>
2015-04-14 12:54:56 +0700

    Review #12

--- hello.py
+++ hello.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 def first_page():
     return render_template("index.html")
 
+# R: нелогичное место для поднятия флага, нужно было это делать
+#     - сразу после создания app
+#     - непосредственно перед вызовом app.run()
+#     - в вызове app.run(debug=True)
 app.debug = True
 if __name__ == '__main__':
     app.run()
