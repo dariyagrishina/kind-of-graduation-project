ce91514eb2bc9aff93c89dd368700a09d235063c
Dariya Grishina <dvgrischina@mail.ru>
2015-04-19 16:01:44 +0700

    Make changes according to review #12

---
+++ app.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from flask import Flask, render_template
+app = Flask(__name__)
+app.debug = True
+
+
+@app.route('/')
+def first_page():
+    return render_template("index.html")
+
+if __name__ == '__main__':
+    app.run()
