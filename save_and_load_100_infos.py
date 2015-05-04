29008cbe65b9f7f54fdb7944a58061645fdc7c88
Alexander Schepanovski <suor.web@gmail.com>
2015-05-04 11:57:10 +0700

    Review #14

--- save_and_load_100_infos.py
+++ save_and_load_100_infos.py
@@ -5,12 +5,15 @@ from get_info import get_info
 
 
 def save_data():
+    # R: стоит использовать map()
     hundred_films = []
     for movie_id in range(1, 101):
         movies_data = get_info(movie_id)
         hundred_films.append(movies_data)
 
     with open("100_movies_data.json", 'w') as f:
+        # R: слишком длинная строка, коммент следовало написать здесь
+        # R: также коммент врёт, т.к. использование ensure_ascii=False ничего не доказывает
         f.write(json.dumps(hundred_films, ensure_ascii=False).encode('utf-8')) # ensure_ascii=False is used just to prove
                                                                                # that everything is OK
 
