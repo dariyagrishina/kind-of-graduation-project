1d637c3131191094f57826f9fd5d1c84b22c9cef
Dariya Grishina <dvgrischina@mail.ru>
2015-05-04 15:17:58 +0700

    Perform task #15

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
 # -*- coding: utf-8 -*-
-from flask import Flask, render_template
+from flask import Flask, request, render_template
 app = Flask(__name__)
-app.debug = True
 
 
 @app.route('/')
 def first_page():
     return render_template("index.html")
 
+
+@app.route('/', methods=['POST'])
+def form_post():
+    keyword = request.form["search_keyword"]
+    return render_template("form_action.html", keyword=keyword)
+
+app.debug = True
 if __name__ == '__main__':
     app.run()
