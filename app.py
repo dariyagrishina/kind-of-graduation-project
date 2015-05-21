9d2bfa2a60e589e44d81e268a3ca5af823c788ae
Dariya Grishina <dvgrischina@mail.ru>
2015-05-21 21:22:15 +0700

    Perform task #18

--- app.py
+++ app.py
@@ -1,12 +1,25 @@
 # -*- coding: utf-8 -*-
 from flask import Flask, request, render_template
+from db import movie_search
 app = Flask(__name__)
 
 
 @app.route('/')
 def first_page():
     search_query = request.args.get('search_query', '')
-    return render_template("index.html", search_query=search_query)
+    movies = movie_search(search_query)
+    number_of_movies = len(movies)
+
+    found = u"Найдено"
+    inflected_movie = u"фильмов"
+    if number_of_movies == 1:
+        found = u"Найден"
+        inflected_movie = u"фильм"
+    if (number_of_movies >= 2) and (number_of_movies <= 4):
+        inflected_movie = u"фильма"
+
+    return render_template("index.html", search_query=search_query, movies=movies,
+                           number_of_movies=number_of_movies, found=found, inflected_movie=inflected_movie)
 
 
 if __name__ == '__main__':
