8fdde91d35bdf4cea55346ba3c8c7065d69336b0
Dariya Grishina <dvgrischina@mail.ru>
2015-05-23 15:19:12 +0700

    Clean cached function

--- cached_func.py
+++ cached_func.py
@@ -5,32 +5,6 @@ from utils import get_page
 from get_info import get_movies_info
 
 
-def _get_path(movie_id):
-    current_path = os.path.dirname(__file__)
-    data_path = os.path.join(current_path, "movies_data/" + str(movie_id))
-    return data_path
-
-
-def _save_info(movie_id, movies_data):
-    with open(_get_path(movie_id), 'w') as f:
-        f.write(json.dumps(movies_data, ensure_ascii=False).encode('utf-8'))
-
-
-def _load_info(movie_id):
-    with open(_get_path(movie_id)) as f:
-        return json.load(f)
-
-
-def cached_get_info(movie_id):
-    if os.path.isfile(_get_path(movie_id)):
-        return _load_info(movie_id)
-    else:
-        movies_data = get_movies_info(movie_id)
-        _save_info(movie_id, movies_data)
-        return movies_data
-
-# cached(func) begin
-
 def _get_file_name(arg):
     return '_'.join(str(ord(c)) for c in str(arg))
 
--- utils.py
+++ utils.py
@@ -15,38 +15,6 @@ def get_page(url):
     return content
 
 
-def get_file_name(url):
-    return '_'.join(str(ord(c)) for c in url)
-
-
-def page_path(url):
-    current_path = os.path.dirname(__file__)
-    page_path = os.path.join(current_path, "cached_pages/" + get_file_name(url))
-    return page_path
-
-
-def save(url, content):
-    cached_page_file = open(page_path(url), "w")
-    cached_page_file.write(content.encode('utf8'))
-    cached_page_file.close()
-
-
-def load(url):
-    cached_page_file = open(page_path(url), "r")
-    content = cached_page_file.read()
-    cached_page_file.close()
-    return content.decode("utf8")
-
-
-def cached_get_page(url):
-    if os.path.isfile(page_path(url)):
-        return load(url)
-    else:
-        content = get_page(url)
-        save(url, content)
-        return content
-
-
 def human_readable(data):
     """
     An utility for print and REPL debugging complex data structures with embedded russian text.
