39fc8982ae2a8c4ba28ac8a7ff45aa67efd93559
Dariya Grishina <dvgrischina@mail.ru>
2015-05-25 19:59:33 +0700

    Perform task #18

--- app.py
+++ app.py
@@ -8,18 +8,18 @@ app = Flask(__name__)
 def first_page():
     search_query = request.args.get('search_query', '')
     movies = movie_search(search_query)
-    number_of_movies = len(movies)
+    return render_template("index.html", search_query=search_query, movies=movies)
 
-    found = u"Найдено"
-    inflected_movie = u"фильмов"
-    if number_of_movies == 1:
-        found = u"Найден"
-        inflected_movie = u"фильм"
-    if (number_of_movies >= 2) and (number_of_movies <= 4):
-        inflected_movie = u"фильма"
 
-    return render_template("index.html", search_query=search_query, movies=movies,
-                           number_of_movies=number_of_movies, found=found, inflected_movie=inflected_movie)
+def plural_filter(number_of_movies, single, plural_1, plural_2):
+    reversed_number = str(number_of_movies)[::-1]
+    if reversed_number[0:1] == '1' and reversed_number[0:2] != "11":
+        return single
+    if reversed_number[0:1] in ["2", "3", "4"] and reversed_number[1:2] !="1":
+        return plural_1
+    return plural_2
+
+app.jinja_env.filters['plural'] = plural_filter
 
 
 if __name__ == '__main__':
